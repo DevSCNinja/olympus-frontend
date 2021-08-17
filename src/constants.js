fe48d825550502bc55303050cd5b8838dbee5350
Unbanksy <unbanksyeth@gmail.com>
2021-08-16 23:53:53 -0600

    Add bond

--- src/constants.js
+++ src/constants.js
@@ -49,8 +49,16 @@ export const addresses = {
     // If we stick to Object.values(PLUTUS_BONDS), it'll simplify the code significantly.
     PLUTUS_BONDS: {
       uni_eth_lp: {
+        bondToken: "UNI-ETH LP",
+        payoutToken: "UNI",
         bondContract: "0x4e99615101ccbb83a462dc4de2bc1362ef1365e5", // UNI-ETH LP
-        payout: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+        payoutTokenContract: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+      },
+      alcx_eth_lp: {
+        bondToken: "ALCX-WETH LP",
+        payoutToken: "ALCX",
+        bondContract: "0xff4D1b6B794A3264Ed151A74d7f1eeDB762f71b0", // ALCX-WETH LP
+        payoutTokenContract: "0x588D273065D9424d8b655015E88746E112fcf3ED",
       },
     },
 
@@ -95,7 +103,10 @@ export const addresses = {
 
     // If we stick to Object.values(PLUTUS_BONDS), it'll simplify the code significantly.
     PLUTUS_BONDS: {
-      uni_eth_lp: {},
+      alcx_eth_lp: {
+        bondContract: "",
+        payout: "0xdbdb4d16eda451d0503b854cf79d55697f90c8df",
+      },
     },
   },
 };
@@ -109,7 +120,7 @@ export const BONDS = {
 };
 
 export const PLUTUS_BONDS = {
-  alcx_eth: "alcx_eth_lp",
+  alcx_eth_lp: "alcx_eth_lp",
   spell_eth: "spell_eth_lp",
   mim_eth: "mim_eth_lp",
   float_eth: "float_eth_lp",
