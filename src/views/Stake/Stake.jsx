25ecfcd1fd4af45f9b8e4bb4e8a0fc909c1f1b83
Bruno Ilponse <bruno.ilponse@gmail.com>
2021-10-12 21:33:22 +0200

    Using submodule for locales

--- src/views/Stake/Stake.jsx
+++ src/views/Stake/Stake.jsx
@@ -232,7 +232,7 @@ function Stake() {
                     {modalButton}
                   </div>
                   <Typography variant="h6">
-                    <Trans>Connect your wallet to stake OHM</Trans>
+                    <Trans>Connect your wallet to stake OHMa</Trans>
                   </Typography>
                 </div>
               ) : (
